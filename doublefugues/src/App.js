f84ca4940aea34ebeab09994050adf28680c7ca6
Luke Bilhorn <lukebilhorn@dagobah.student.wheaton.edu>
2024-04-25 22:19:02 -0500

    Something is going wrong here. Another test.

--- doublefugues/src/App.js
+++ doublefugues/src/App.js
@@ -1,5 +1,10 @@
 import React, { useRef, useState } from 'react';
+import reactDOM from 'react-dom';
 import './App.css';
+/* icon imports */
+import { Chat, Help, RssFeed, Search, Label } from "@mui/icons-material";
+/* image imports */
+import background from "./components/assets/background.jpg";
 
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
@@ -8,6 +13,11 @@ import 'firebase/compat/analytics'
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+
+
+
+
 
 firebase.initializeApp({
   apiKey: "AIzaSyABZ_Vo1gmStINDqZfuDMvMKe1POot0jGU",
@@ -23,19 +33,14 @@ const auth = firebase.auth();
 const firestore = firebase.firestore();
 const analytics = firebase.analytics();
 
+
+
 function App() {
-
   const [user] = useAuthState(auth);
-
   return (
     <div className="App">
-      <header>
-        <h1>DoubleFugues</h1>
-        <SignOut />
-      </header>
-
       <section>
-        {user ? <PostsPage /> : <SignIn />}
+        {user ? <Profile /> : <SignIn />}
       </section>
     </div>
   );
@@ -65,9 +70,11 @@ function SignIn() {
   function PostsPage() {
 
     const dummy = useRef();
+    const { uid } = auth.currentUser;
 
     const postsRef = firestore.collection('posts');
     const query = postsRef.orderBy('createdAt');
+        /*const query = postsRef.where('uid','==',uid);*/
 
     const [posts] = useCollectionData(query, {idField: 'id'});
 
@@ -112,14 +119,145 @@ function SignIn() {
 
   function SinglePost(props) {
     const { text, uid, photoURL, createdAt } = props.posts;
-
     const postClass = uid === auth.currentUser.uid ? 'sent' : 'received';
     return (
-      <div className={'post ${postClass}'}>
+      <div className="shareBox"> 
+                <div className="shareWrapper">
+                    <div className="shareTop">
+                        <img className="shareProfileImg" src={photoURL} alt="" />
+                        <p>
+                          {text}
+                        </p>
+                    </div>
+                    <hr className="shareHr"/>
+                    <div className="shareBottom">
+                        <div className="shareOptions">
+                            {/* shareOption is current placeholder for tag system */}
+                            <div className="shareOption">
+                                <Label className="shareIcon"/>
+                                <span className="shareOptionText">Tag</span>
+                            </div>
+                            <div className="shareOption">
+                                <Label className="shareIcon"/>
+                                <span className="shareOptionText">Tag</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            /*<div className={'post ${postClass}'}>
         <img src={photoURL} />
         <p>{text}</p>
-      </div>
+      </div>*/
     )
   }
 
-export default App;
+
+function Profile() {
+  var firstname, lastname;
+  const { uid, photoURL } = auth.currentUser;
+  const profileRef = firestore.collection('account');
+  const query = profileRef.where('uid','==',uid);
+  const [profile] = useCollectionData(query, { idField: 'id' });
+  const doc = profileRef.get();
+  if (!doc.exists) {
+    firstname = 'firstname';
+    lastname = 'lastname';
+  } else {
+    var data = doc.data();
+    firstname = data.fname;
+    lastname = data.lname;
+  }
+    
+
+  return(
+      <>
+          <Topbar className="topbar"/>
+          <div className="profile">
+              <Sidebar className="sidebar"/>
+              <div className="profileRight">
+                  <div className="profileRightTop">
+                      <img className="profileCoverImg" src = {background} alt=""/>
+                      <img className="profileUserImg" src = {photoURL} alt=""/>
+                      <div className="profileInfo">
+                          
+                          <h4 className="profileInfoName">
+                            {firstname} {lastname}
+                          </h4>
+                          
+                          <span className="profileInfoDesc">description</span>
+                      
+                      </div>
+                  </div>
+                  <div className="profileRightBottom">
+                      <PostsPage/>
+                  </div>
+              </div>   
+          </div>
+      </>
+  )
+}
+
+function Topbar() {
+  return (
+    <div className="topbarContainer">
+        <div className="topbarLeft">
+            <span className="logo">
+              Double Fugue
+            </span>
+        </div>
+        <div className="topbarCenter">
+            <div className="searchbar">
+                <Search className="searchIcon"/>
+                <input placeholder="Search" className="searchInput"/>
+            </div>
+        </div>
+        <div className="topbarRight">
+            <div className="topbarLinks">
+                <span className="topbarLink">
+                  About Double Fugue
+                </span>
+                <span className="topbarLink">
+                  FAQ
+                </span>
+                <span className="topbarLink">
+                  Home
+                </span>
+                <SignOut className="signOutButton"/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+function Sidebar() {
+  return(
+      <div className="sidebar">
+          <div className="sidebarWrapper">
+              <ul className="sidebarList">
+                  <li className="sidebarListItem">
+                    {/* Feed not currently implemented, may be scrapped */}
+                      <RssFeed className="sidebarIcon"/>
+                      <span className="sidebarListItemText">
+                        Feed
+                      </span>
+                  </li>
+                  <li className="sidebarListItem">
+                      <Chat className="sidebarIcon"/>
+                      <span className="sidebarListItemText">Chats</span>
+                  </li>
+                  <li className="sidebarListItem">
+                      <Help className="sidebarIcon"/>
+                      <span className="sidebarListItemText">Help</span>
+                  </li>
+              </ul>
+              <button className="sidebarButton">
+                Show More
+              </button>
+              <hr/>
+          </div>
+      </div>)
+}
+
+
+  export default App;
