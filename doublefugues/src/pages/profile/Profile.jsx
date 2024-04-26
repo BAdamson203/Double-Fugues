dbbeded675fbc90567e2095e7fb7afdf396cd5a8
Luke Bilhorn <lukebilhorn@dagobah-2.student.wheaton.edu>
2024-04-25 23:46:28 -0500

    Changed bar color change speed, added other files

---
+++ doublefugues/src/pages/profile/Profile.jsx
@@ -0,0 +1,34 @@
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import "./profile.css";
+import Background from "../../components/assets/background.jpg";
+import Person from "../../components/assets/defaultAvatar.jpg";
+
+
+export default function Profile() {
+    return(
+        <>
+            <Topbar className="topbar"/>
+            <div className="profile">
+                <Sidebar className="sidebar"/>
+                <div className="profileRight">
+                    <div className="profileRightTop">
+                        <img className="profileCoverImg" src = {Background} alt=""/>
+                        <img className="profileUserImg" src = {Person} alt=""/>
+                        <div className="profileInfo">
+                            
+                            <h4 className="profileInfoName">name</h4>
+                            
+                            <span className="profileInfoDesc">description</span>
+                        
+                        </div>
+                    </div>
+                    <div className="profileRightBottom">
+                        <Feed/>
+                    </div>
+                </div>   
+            </div>
+        </>
+    )
+}
